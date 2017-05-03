3a51f3d30ab23a48146c2d14c5dffad5c93fbe61
ZavbiA <anja.zavbi@gmail.com>
2017-05-03 09:18:21 +0200

    Create uvoz_tabela1.r
    osnutek funkcij uvoza vseh 3 tabel

---
+++ uvoz/uvoz_tabela1.r
@@ -0,0 +1,28 @@
+# 2. faza: Uvoz podatkov
+
+# Funkcija, ki uvozi število medalj po državah iz Wikipedije
+  uvozi.medalje <- function() {
+  link <- "https://en.wikipedia.org/wiki/All-time_Olympic_Games_medal_table"
+  stran <- html_session(link) %>% read_html()
+  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+  .[[1]] %>% html_table(dec = ".")
+  colnames(tabela) <- c("države", "zimske_bronaste", "zimske_srebrne", "zimske_zlate", "poletne_bronaste",                        
+  "poletne_srebrne", "poletne_zlate")  
+  return(tabela)}
+  
+View(tabela)
+  
+# Funkcija, ki uvozi podatke iz datoteke populacija.csv
+  uvozi.populacija <- function(populacija) {  
+  return(data)}
+  
+# Zapišimo podatke v razpredelnico medalje
+  medalje <- uvozi.medalje()
+# Zapišimo podatke v razpredelnico slovenija
+  slovenija <- uvozi.slovenija()
+# Zapišimo podatke v razpredelnico populacija
+  populacija <- uvozi.populacija(levels(populacija$populacija))
+  
+  
+  
+  
