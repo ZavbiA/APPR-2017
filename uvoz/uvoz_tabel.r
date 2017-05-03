6a7436d26bdf1073bd4ba577dc7bd98f7c846c60
ZavbiA <anja.zavbi@gmail.com>
2017-05-03 09:47:15 +0200

    Update uvoz_tabel.r
    uvožena prva tabela

--- uvoz/uvoz_tabel.r
+++ uvoz/uvoz_tabel.r
@@ -1,28 +1,28 @@
 # 2. faza: Uvoz podatkov
 
+library(rvest)
+library(gsubfn)
+library(readr)
+library(dplyr)
+
 # Funkcija, ki uvozi število medalj po državah iz Wikipedije
-uvozi.medalje <- function() {
-  link <- "https://en.wikipedia.org/wiki/All-time_Olympic_Games_medal_table"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-  .[[1]] %>% html_table(dec = ".")
-  colnames(tabela) <- c("države", "zimske_bronaste", "zimske_srebrne", "zimske_zlate", "poletne_bronaste",                        
-  "poletne_srebrne", "poletne_zlate")  
-  return(tabela)}
+link <- "https://en.wikipedia.org/wiki/All-time_Olympic_Games_medal_table"
+stran <- html_session(link) %>% read_html()
+tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+.[[1]] %>% html_table(dec = ".")
+
+#pobrišem odvečne stolpce in vrstice
+tabela[2] <- NULL
+tabela[5] <- NULL
+tabela[5] <- NULL
+tabela[8] <- NULL
+tabela[8] <- NULL
+tabela[8] <- NULL
+tabela[8] <- NULL
+tabela[8] <- NULL
+tabela[8] <- NULL
+
+colnames(tabela) <- c("države", "zimske_bronaste", "zimske_srebrne", "zimske_zlate", "poletne_bronaste",                        
+"poletne_srebrne", "poletne_zlate")
   
 View(tabela)
-  
-# Funkcija, ki uvozi podatke iz datoteke populacija.csv
-uvozi.populacija <- function(populacija) {  
-  return(data)}
-  
-# Zapišimo podatke v razpredelnico medalje
-  medalje <- uvozi.medalje()
-# Zapišimo podatke v razpredelnico slovenija
-  slovenija <- uvozi.slovenija()
-# Zapišimo podatke v razpredelnico populacija
-  populacija <- uvozi.populacija(levels(populacija$populacija))
-  
-  
-  
-  
