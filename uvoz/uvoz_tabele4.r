153798a85cd5233d9a1210e1ce4f3565c5bf7cad
ZavbiA <anja.zavbi@gmail.com>
2017-06-05 15:51:47 +0200

    popravljeno ime zadnjega stolpca

--- uvoz/uvoz_tabele4.r
+++ uvoz/uvoz_tabele4.r
@@ -11,8 +11,8 @@ stran4 <- html_session(link4) %>% read_html()
 tabela4 <- stran4 %>% html_nodes(xpath="//table[@class='wikitable']") %>% .[[1]] %>% html_table(fill = TRUE)
 names(tabela4)[c(1, 3, 5)] <- c("leto", "kraj", "kraj")
 
-tabela4.tidy <- rbind(tabela4[-1, c(1, 3)] %>% mutate(igre = "poletne"),
-                      tabela4[-1, c(1, 5)] %>% mutate(igre = "zimske")) %>%
+tabela4.tidy <- rbind(tabela4[-1, c(1, 3)] %>% mutate(tip = "poletne"),
+                      tabela4[-1, c(1, 5)] %>% mutate(tip = "zimske")) %>%
   filter(kraj != "", ! grepl("Cancelled", kraj))
 Encoding(tabela4.tidy$kraj) <- "UTF-8"
 
@@ -22,4 +22,4 @@ tabela4.tidy$kraj <- gsub("^.*\\n ", "", tabela4.tidy$kraj)
 tabela4.tidy <- tabela4.tidy %>% transmute(leto = parse_number(leto),
                                            mesto = kraj %>% strapplyc("^(.*),") %>% unlist(),
                                            drzava = kraj %>% strapplyc(", (.*)$") %>% unlist(),
-                                           igre)
+                                           tip)
