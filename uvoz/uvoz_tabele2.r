4f027624f75498ffedc47f255383f797968985e0
ZavbiA <anja.zavbi@gmail.com>
2017-06-06 19:24:57 +0200

    preimenovana "Slovak Republic" v "Slovakia"

--- uvoz/uvoz_tabele2.r
+++ uvoz/uvoz_tabele2.r
@@ -41,3 +41,5 @@ tabela2.tidy <- melt(data, value.name = "stevilo_prebivalcev")
 names(tabela2.tidy) <- c("drzava","leto","stevilo_prebivalcev")
 tabela2.tidy$leto <- parse_number(tabela2.tidy$leto)
 tabela2.tidy$stevilo_prebivalcev <- parse_number(tabela2.tidy$stevilo_prebivalcev)
+
+tabela2.tidy$drzava <- gsub("Slovak Republic", "Slovakia", tabela2.tidy$drzava)
