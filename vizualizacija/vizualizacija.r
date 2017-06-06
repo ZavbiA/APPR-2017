8eda92aa3cc8244aff463cc84042ead53968724d
ZavbiA <anja.zavbi@gmail.com>
2017-06-06 13:40:21 +0200

    Update vizualizacija.r

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -7,9 +7,14 @@ gpclibPermit()
 library(rvest)
 library(gsubfn)
 library(readr)
-library(dplyr) 
+library(dplyr)
+library(ggplot2)
+library(tibble)
 
 # Uvozim zemljevid.
 zemljevid <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
                              "ne_50m_admin_0_countries", encoding = "Windows-1252") %>%
   pretvori.zemljevid()
+
+ggplot() + geom_polygon(data = zemljevid, aes(x=long, y=lat, group=group, fill=id)) +
+  guides(fill = FALSE)
