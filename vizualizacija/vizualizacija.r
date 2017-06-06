dacda873b7e4b64d15aa4ea510ae14fc9b144e6c
ZavbiA <anja.zavbi@gmail.com>
2017-06-06 13:25:57 +0200

    uvoz zemljevida

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,5 +1,14 @@
 # 3. faza: Vizualizacija podatkov
 
+library(sp)
+library(maptools)
+library(digest)
+gpclibPermit()
+library(rvest)
+library(gsubfn)
+library(readr)
+library(dplyr) 
+
 # Uvozim zemljevid.
 zemljevid <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
                              "ne_50m_admin_0_countries", encoding = "Windows-1252") %>%
