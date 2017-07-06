d9b089e8bd28db77c93c0234ec5b01e8fc99df45
ZavbiA <anja.zavbi@gmail.com>
2017-07-06 15:31:51 +0200

    dodana peta tabela

---
+++ uvoz/uvoz_tabele5.r
@@ -0,0 +1,18 @@
+library(rvest) 
+library(gsubfn) 
+library(readr) 
+library(dplyr) 
+library(tibble)
+library(reshape2)
+library(gsubfn)
+library(tidyr)
+
+tabela5.tidy <- read_csv("podatki/stroski.csv",
+                  locale = locale(encoding = "UTF-8")) 
+
+names(tabela5.tidy) <- c("mesto","leto","drzava","tip","st_dogodkov","st_nastopajocih","cena_bilion_USD")
+tabela5.tidy <- tabela5[-c(1,17,18,34,35), ]
+tabela5.tidy$tip <- gsub("Summer", "P", tabela5.tidy$tip)
+tabela5.tidy$tip <- gsub("Winter", "Z", tabela5.tidy$tip)
+
+tabela5.tidy$cena_bilion_USD <- parse_number(tabela5.tidy$cena_bilion_USD)
