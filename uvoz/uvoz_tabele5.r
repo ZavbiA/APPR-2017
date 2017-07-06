bf2b7f9b675dfb5e7a17d347dfa38281a130f63c
ZavbiA <anja.zavbi@gmail.com>
2017-07-06 16:23:05 +0200

    Update uvoz_tabele5.r

--- uvoz/uvoz_tabele5.r
+++ uvoz/uvoz_tabele5.r
@@ -10,7 +10,7 @@ library(tidyr)
 tabela5.tidy <- read_csv("podatki/stroski.csv",
                   locale = locale(encoding = "UTF-8")) 
 
-names(tabela5.tidy) <- c("mesto","leto","drzava","tip","st_dogodkov","st_nastopajocih","cena_bilion_USD")
+names(tabela5.tidy) <- c("mesto","leto","drzava","tip","st_dogodkov","st_nastopajocih","stroski")
 tabela5.tidy <- tabela5[-c(1,17,18,34,35), ]
 tabela5.tidy$tip <- gsub("Summer", "P", tabela5.tidy$tip)
 tabela5.tidy$tip <- gsub("Winter", "Z", tabela5.tidy$tip)
