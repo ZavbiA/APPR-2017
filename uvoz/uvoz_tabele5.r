3fd269846228ca9e3e025e15ec9024cddf702a4f
ZavbiA <anja.zavbi@gmail.com>
2017-07-07 11:39:30 +0200

    popravljen uvoz tabele

--- uvoz/uvoz_tabele5.r
+++ uvoz/uvoz_tabele5.r
@@ -11,7 +11,9 @@ tabela5.tidy <- read_csv("podatki/stroski.csv",
                   locale = locale(encoding = "UTF-8")) 
 
 names(tabela5.tidy) <- c("mesto","leto","drzava","tip","st_dogodkov","st_nastopajocih","stroski")
-tabela5.tidy <- tabela5[-c(1,17,18,34,35), ]
+tabela5.tidy <- tabela5.tidy %>% filter(! mesto %in% c("Average", "Median"))
+tabela5.tidy <- tabela5.tidy[-c(1), ]
 tabela5.tidy$tip <- gsub("Summer", "P", tabela5.tidy$tip)
 tabela5.tidy$tip <- gsub("Winter", "Z", tabela5.tidy$tip)
 tabela5.tidy$stroski <- parse_number(tabela5.tidy$stroski)
+tabela5.tidy$leto <- parse_number(tabela5.tidy$leto)
