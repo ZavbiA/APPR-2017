d5ac37029d0adf50b757c3378909f6c033d08069
ZavbiA <anja.zavbi@gmail.com>
2017-07-11 16:30:41 +0200

    Update ui.R

--- shiny/ui.R
+++ shiny/ui.R
@@ -6,7 +6,8 @@ fluidPage(
   
   sidebarLayout(
       sidebarPanel(
-        selectInput("leto", "Leto:", choises = c(2000,2002,2004,2006,2008,2010,2012,2014,2016)),
+        selectInput("leto", "Leto:", choises = c(2000,2002,2004,2006,2008,2010,2012,2014,2016),
+                    multiple = TRUE, selected = c(2000)),
         hr(),
         helpText()
       ),
