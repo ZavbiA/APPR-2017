8d44026578a781eeeca5478703d07911d439dcb7
ZavbiA <anja.zavbi@gmail.com>
2017-07-11 16:33:22 +0200

    osnutek

--- shiny/server.R
+++ shiny/server.R
@@ -1,24 +1,10 @@
 library(shiny)
 
-shinyServer(function(input, output) {
-  output$druzine <- DT::renderDataTable({
-    dcast(druzine, obcina ~ velikost.druzine, value.var = "stevilo.druzin") %>%
-      rename(`Občina` = obcina)
+function(input,output) {
+  output$graf_medalj <- renderPlot({
+    barplot(skupna_tabela[,input$leto],
+            main=input$leto,
+            ylab="Število osvojenih medalj",
+            xlab="Država")
   })
-  
-  output$pokrajine <- renderUI(
-    selectInput("pokrajina", label="Izberi pokrajino",
-                choices=c("Vse", levels(obcine$pokrajina)))
-  )
-  output$naselja <- renderPlot({
-    main <- "Pogostost števila naselij"
-    if (!is.null(input$pokrajina) && input$pokrajina %in% levels(obcine$pokrajina)) {
-      t <- obcine %>% filter(pokrajina == input$pokrajina)
-      main <- paste(main, "v regiji", input$pokrajina)
-    } else {
-      t <- obcine
-    }
-    ggplot(t, aes(x = naselja)) + geom_histogram() +
-      ggtitle(main) + xlab("Število naselij") + ylab("Število občin")
-  })
-})
+}
