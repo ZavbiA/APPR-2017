836731cf28310c542f794b0fb6cde1a3e955dadc
ZavbiA <anja.zavbi@gmail.com>
2017-07-11 16:43:49 +0200

    Update server.R

--- shiny/server.R
+++ shiny/server.R
@@ -1,5 +1,14 @@
 library(shiny)
 
+if ("server.R" %in% dir()) {
+  setwd("..")
+}
+
+source("lib/libraries.r", encoding = "UTF-8")
+source("uvoz/uvoz_tabele4.r", encoding = "UTF-8")
+source("uvoz/uvoz_tabele5.r", encoding = "UTF-8")
+source("uvoz/uvoz_shiny.r", encoding = "UTF-8")
+
 function(input,output) {
   output$graf_medalj <- renderPlot({
     barplot(skupna_tabela[,input$leto],
