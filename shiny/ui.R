a85facdd4e8d5a5324f8ddf565d3df85e29477a0
ZavbiA <anja.zavbi@gmail.com>
2017-07-12 18:57:28 +0200

    popravljene slovnične napake

--- shiny/ui.R
+++ shiny/ui.R
@@ -6,12 +6,12 @@ shinyUI(fluidPage(
   
   sidebarLayout(
       sidebarPanel(
-        selectInput("leto", "Leto:", choises = c(2000,2002,2004,2006,2008,2010,2012,2014,2016),
+        selectInput("leto", "Leto:", choices = c(2000,2002,2004,2006,2008,2010,2012,2014,2016),
                     multiple = TRUE, selected = c(2000)),
         hr(),
         helpText()
       ),
-      mailPanel(
+      mainPanel(
         plotOutput("graf_medalj")
       )
   )
